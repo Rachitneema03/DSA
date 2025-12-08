2c3c5ada9489eedbf2c49590e601a575c3699453
Rachit <rachitneemawork@gmail.com>
2025-12-08 19:12:39 +0530

    Time: 21 ms (27.45%) | Memory: 80 MB (49.20%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,12 +1,5 @@
 class Solution {
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {
-            }
-        }));
-         }
+   
     public boolean containsDuplicate(int[] nums) {
         HashSet <Integer> list = new HashSet<>();
         for(int i : nums){
