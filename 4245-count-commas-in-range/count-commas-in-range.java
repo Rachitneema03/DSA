5e39a6e1a899c35faea388b79989f45d6d1e2e08
Rachit <rachitneemawork@gmail.com>
2026-03-15 23:49:52 +0530

    Time: 1 ms (87.49%) | Memory: 42.7 MB (38.34%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,10 +1,7 @@
 class Solution {
     public int countCommas(int n) {
-        int comma = 0;
-        while(n >= 1000){
-            comma++;
-            n--;
-        }
-        return comma;
+        if(n < 999) return 0;
+        else
+        return (n-999);
     }
 }
