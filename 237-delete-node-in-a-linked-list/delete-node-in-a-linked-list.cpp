cfe50277cb568e693c46fd907d217565ec136cc3
Rachit <rachitneemawork@gmail.com>
2026-03-20 23:53:33 +0530

    Time: 7 ms (54.56%) | Memory: 12.4 MB (21.81%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
+        // Копируем данные из следующего узла в текущий
         node->val = node->next->val;
+        // Переставляем указатель, пропуская следующий узел
         node->next = node->next->next;
     }
-};
+}
+;
