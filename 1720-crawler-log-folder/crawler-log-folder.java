b5b3527d645bb25754a01c890fc1f3f1d637be35
Rachit <rachitneemawork@gmail.com>
2026-07-13 12:09:55 +0530

    Time: 2 ms (49.54%) | Memory: 43.5 MB (97.67%) - LeetSync

---
+++ 1720-crawler-log-folder/crawler-log-folder.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> stack = new Stack<>();
+
+        for(String s : logs){
+            if(s.equals("../")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }
+            else if(s.equals("./")){
+                continue;
+            }
+            else{
+                stack.push(s);
+            }
+        }
+        return stack.size();
+    }
+}
